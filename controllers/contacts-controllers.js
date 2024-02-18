1ad6843004455925de48f6fdab80d7386458a204
bogdansolomich <bogdansolomich@gmail.com>
2024-02-18 18:08:55 +0200

    отпр

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -15,22 +15,11 @@ const getAllContacts = async (req, res, next) => {
   }
 };
 
-// const getContactById = async (req, res, next) => {
-//   const { contactId } = req.params;
-//   const contact = await contactsService.getContactById(contactId);
-//   if (!contact) {
-//     throw HttpError(404, "Not found");
-//   }
-//   res.json(contact);
-// };
-
 const getContactById = async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const contact = await contactsService.getContactById(contactId);
-    if (!contact) {
-      throw HttpError(404);
-    }
+    if (!contact) return res.status(404).json({ message: "Not found" });
     res.status(200).json(contact);
   } catch (error) {
     next(error);
@@ -39,9 +28,26 @@ const getContactById = async (req, res, next) => {
 
 const addNewContact = async (req, res, next) => {
   const newContact = await contactsService.addContact(req.body);
-
+  if (!newContact) return res.status(404).json(newContact);
   res.status(201).json(newContact);
 };
+
+// const addNewContact = async (req, res, next) => {
+//   const { name, email, phone } = req.body;
+
+//   try {
+//     const result = await contactsService.addContact({ name, email, phone });
+
+//     if (!result) {
+//       throw HttpError(400, "Not Found");
+//     }
+
+//     res.status(201).json(result);
+//   } catch (error) {
+//     console.log(error);
+//     next(error);
+//   }
+// };
 
 const updateContactById = async (req, res, next) => {
   const { contactId } = req.params;
@@ -71,9 +77,7 @@ const deleteContactById = async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const deletedContact = await contactsService.removeContact(contactId);
-    if (!deletedContact) {
-      throw HttpError(404);
-    }
+    if (!deletedContact) return res.status(404).json({ message: "Not found" });
     res.status(200).json(deletedContact);
   } catch (error) {
     next(error);
